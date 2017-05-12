fe8d52e8734bdc9b7a4f57fcbace0f7eac5412b1
Nick Malleson <n.s.malleson@leeds.ac.uk>
2017-05-12 12:22:32 +0100

    Rebuilt project for scala build tool

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "filmguide"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "3.0.0"
+
+libraryDependencies += "org.jsoup" % "jsoup" % "1.6.1"
---
+++ src/main/scala/filmguide/Main.scala
@@ -0,0 +1,52 @@
+package filmguide
+
+/**
+  * Created by nick on 30/04/2017.
+  */
+object Main {
+
+  /**
+    * Application entry point
+    * @param args
+    */
+  def main( args:Array[String] ):Unit = {
+
+    val parser = BlebParser()
+    val programmes = parser.getProgrammes()
+
+
+  }
+
+}
+
+/**
+  * Store information about a TV programme or film
+  * Created by nick on 30/04/2017.
+  *
+  * @constructor Creates a new programme with a name and scheduled date
+  * @param name The name of the programme
+  * @param scheduled The date that the programme is scheduled to take place
+  */
+case class Program ( name: String, scheduled:String) {
+
+
+}
+
+package filmguide
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Super class for the code to parse the TV listing information
+  * Created by nick on 30/04/2017.
+  */
+trait ScheduleParser {
+
+  /**
+    * Get the schedule and parse it, returning an array of Program objects.
+    */
+  def getProgrammes(): ArrayBuffer[Program]
+
+
+
+}
